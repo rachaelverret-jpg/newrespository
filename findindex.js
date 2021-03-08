e74868b2e6be4beebe11645ffafc3e59aa7afb0c
rachaelverret-jpg <74328213+rachaelverret-jpg@users.noreply.github.com>
2021-03-08 16:14:20 -0500

    Add files via upload

---
+++ findindex.js
@@ -0,0 +1,15 @@
+const users = [
+    {username: 'codeiscool'},
+    {username: 'darkknight'},
+    {username: 'smith123'}
+];
+
+findByUsername(users, 'codeiscool')
+findByUsername(users, 'taco')
+
+const users = [
+    {username: 'codeiscool'},
+    {username: 'darkknight'},
+    {username: 'smith123'}
+];
+removeUser(users, 'smith123')
