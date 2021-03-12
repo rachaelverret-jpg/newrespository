072e12dc57fa1b79b0806c5ac7bcbbc2f9b1d0c5
rachaelverret-jpg <74328213+rachaelverret-jpg@users.noreply.github.com>
2021-03-12 18:49:49 -0500

    Add files via upload

---
+++ objectenhancements.js
@@ -0,0 +1,65 @@
+//Same keys and values
+
+function createInstructor(firstName, lastName){
+    return {
+      firstName: firstName,
+      lastName: lastName
+    }
+  }
+
+  //Same keys and values ES2015
+  
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    }
+  }
+  
+//Computed Property Names
+
+var favoriteNumber = 42;
+
+var instructor = {
+  firstName: "Colt"
+}
+
+instructor[favoriteNumber] = "That is my favorite!"
+
+//Computed Property Names ES2015
+let favoriteNumber = 42;
+
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+
+  //Object Mtheods ES2015
+  const instructor = {
+    firstName: "Colt",
+    sayHi(){
+      return "Hi!";
+    },
+    sayBye(){
+      return this.firstName " says bye!";
+    }
+  }
+
+//createAnimal function
+
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise){
+  return {
+    species,
+    [verb](){
+      return noise;
+    }
+  }
+}
